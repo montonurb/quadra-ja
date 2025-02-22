7a387ba68671eb6c2a70fea80066d576e822d172
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2025-02-21 23:25:52 -0300

    Adicionando funcao de desabilitar e buscar todos

--- api/src/main/java/br/com/quadraja/api/repositories/SportRepository.java
+++ api/src/main/java/br/com/quadraja/api/repositories/SportRepository.java
@@ -1,11 +1,17 @@
 package br.com.quadraja.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.quadraja.api.models.Sport;
 
 @Repository
 public interface SportRepository extends JpaRepository<Sport, Long> {
+
+    @Query("SELECT s FROM Sport s WHERE s.active = TRUE")
+    List<Sport> findAllActive();
 
 }
