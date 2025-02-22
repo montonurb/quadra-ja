cfcd39c633c45b22ea0a8e9ef793f7d18e1de302
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2025-02-21 23:27:11 -0300

    Adicionando funcao de desabilitar e buscar todos

--- api/src/main/java/br/com/quadraja/api/repositories/UserRepository.java
+++ api/src/main/java/br/com/quadraja/api/repositories/UserRepository.java
@@ -1,6 +1,10 @@
 package br.com.quadraja.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.quadraja.api.models.User;
@@ -8,4 +12,12 @@ import br.com.quadraja.api.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u FROM User u WHERE u.active = TRUE AND u.email LIKE :email AND u.password LIKE :password")
+    List<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    @Query("SELECT u FROM User u WHERE u.active = TRUE AND u.email LIKE :email")
+    List<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.active = TRUE")
+    List<User> findAllActive();
 }
