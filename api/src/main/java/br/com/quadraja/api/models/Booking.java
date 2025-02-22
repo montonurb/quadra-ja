2e874e9e684b47c382435862d2e5a9a4de85154a
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2025-02-21 23:24:01 -0300

    Criando classe Booking

--- api/src/main/java/br/com/quadraja/api/models/Booking.java
+++ api/src/main/java/br/com/quadraja/api/models/Booking.java
@@ -3,9 +3,11 @@ package br.com.quadraja.api.models;
 import java.time.LocalTime;
 
 import br.com.quadraja.api.enums.Status;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,15 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity
+@Table(name = "bookings")
 public class Booking {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private boolean active;
     private LocalTime startTime;
     private LocalTime endTime;
     private Status status;
+    private LocalTime registrationDate;
 }
