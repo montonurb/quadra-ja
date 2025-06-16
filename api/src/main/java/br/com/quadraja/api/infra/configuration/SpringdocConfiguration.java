d63380577227ec288d75cdd1f8dfeda633ef499b
montonurb <brunomoncar007@gmail.com>
2025-06-16 16:19:25 -0300

    Adicionando campo token dentro da consulta do swagger

--- api/src/main/java/br/com/quadraja/api/infra/configuration/SpringdocConfiguration.java
+++ api/src/main/java/br/com/quadraja/api/infra/configuration/SpringdocConfiguration.java
@@ -1,21 +1,30 @@
-//package br.com.quadraja.api.infra.configuration;
+package br.com.quadraja.api.infra.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class SpringdocConfiguration {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .components(new Components()
-//                        .addSecuritySchemes("bearer-key",
-//                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
-//    }
-//
-//}
+@Configuration
+public class SpringdocConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Quadraja API")
+                        .description("API REST da aplicação Quadraja, contendo as informações do CRUD.")
+                        .contact(new Contact().name("Backend Group").email("backend@quadraja.com")).license(new License().name("Apache 2.0").url("https://www.quadraja.org/api/licenca")));
+    }
+}
 
